4c24007128388e8e411f11f42057dc3da2d5289b
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-09 09:52:36 -0800

    Refactor to use factory bot

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  let(:region) { Region.new }
+  let(:region) { FactoryBot.build_stubbed(:region) }
 
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
 
   it "has a string representation that is its name" do
     name = 'Mt. Hood'
-    region = Region.new(name: name)
     result = region.to_s
   end
 
