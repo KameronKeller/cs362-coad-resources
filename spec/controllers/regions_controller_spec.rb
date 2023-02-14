aa201abf08db7150cdd897f31712835e4848655e
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-14 11:19:46 -0800

    Add notes for writing tests

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,46 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+    # it "returns all regions" do
+    #     region_controller = RegionsController.new
+    #     puts "printing here:"
+    #     puts region_controller.index.length()
+    end
+
 end
+
+
+#     describe "get stuff" do
+
+#         it "shows regions when user is admin" do
+
+#         end
+
+#         it "shows regions when user is user" do
+
+#         end
+
+#         it "does not show regions when not a user" do
+        
+#         end
+
+
+            
+
+# end
+
+
+# RSpec.describe TeamsController do
+#     describe "GET index" do
+#       it "assigns @teams" do
+#         team = Team.create
+#         get :index
+#         expect(assigns(:teams)).to eq([team])
+#       end
+  
+#       it "renders the index template" do
+#         get :index
+#         expect(response).to render_template("index")
+#       end
+#     end
+#   end
