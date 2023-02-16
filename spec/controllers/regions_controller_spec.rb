ea3763a1595cca9670d0dd55f2c5557af3546527
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-16 11:53:52 -0800

    Remove unneeded comments

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -60,84 +60,11 @@ RSpec.describe RegionsController, type: :controller do
       end 
 
       describe "POST #create" do
-        # let(:region) { create(:region) }
-        #can't have region of the same name, creating one here and in the post request
-
         it {
-          # post(:create, params: { region: { :name => region.name } })
-          
-          # expect(response).to be_successful
-          # ^ this works by itself
-
           post(:create, params: { region: attributes_for(:region) })
           expect(response).to redirect_to(regions_path)
         }
       end
     end
 
-
-    #params is hash, all data passed to this function
-    #want tickets, find ID
-
-    
-    
- 
-
-
-    #Redirects to dashboard path found in application controller
-    #actually redirects to signin page
-
-    
-    # it "returns all regions" do
-    #     region_controller = RegionsController.new
-    #     puts "printing here:"
-    #     puts region_controller.index.length()
-    # end
-        # describe "GET index" do
-
-        #   it "contains the regions" do
-        #     region = Region.create
-        #     get "index"
-        #     # region.name = "ASDF"
-        #     expect(response).to include region
-        #   end
-
-        # end
-
 end
-
-
-#     describe "get stuff" do
-
-#         it "shows regions when user is admin" do
-
-#         end
-
-#         it "shows regions when user is user" do
-
-#         end
-
-#         it "does not show regions when not a user" do
-        
-#         end
-
-
-            
-
-# end
-
-
-# RSpec.describe TeamsController do
-#     describe "GET index" do
-#       it "assigns @teams" do
-#         team = Team.create
-#         get :index
-#         expect(assigns(:teams)).to eq([team])
-#       end
-  
-#       it "renders the index template" do
-#         get :index
-#         expect(response).to render_template("index")
-#       end
-#     end
-#   end
