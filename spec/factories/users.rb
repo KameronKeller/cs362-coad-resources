551f79e62f693fecf03707620d75c703543e3e8b
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-16 11:53:18 -0800

    Add traits and update role

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :user do
-        role { :admin }
+        #role { :organization }
         #email { 'admin_user@test.com' }
 
         sequence :email do |n|
@@ -8,9 +8,27 @@ FactoryBot.define do
         end
 
         password { '1234Testing!' }
+
+        before(:create) { |user| user.skip_confirmation!}
+
+        trait :organization_approved do #added in organization approved/unapproved traits based on class example
+            role {:organization}
+            organization_id {create(:organization, :approved).id }
+        end
+        
+        trait :organization_unapproved do
+            role {:organization}
+            organization_id {create(:organization.id) }
+        end        
+
+        trait :admin do
+            role { :admin }
+        end
     end
 
   end
+
+
 
 
 #   sequence :name do |n|
