98566f3ba612f8707ed0a681883131475ece219a
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-23 15:14:25 -0800

    Remove unneeded comments

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -7,10 +7,9 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         let(:user) { create(:user) }
         before(:each) { sign_in(user) }
 
-        #before_action :set_resource_category, except: [:index, :new, :create] Does this need to be replicated?
   
         describe "GET #index" do
-          it { expect(get(:index)).to redirect_to(dashboard_path) } #what about checking in order?
+          it { expect(get(:index)).to redirect_to(dashboard_path) }
         end
   
         describe "GET #new" do
@@ -49,16 +48,11 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       #LOGGED OUT
       context 'as a logged-out user' do
         let(:user) { create(:user) }
-        #before_action :set_resource_category, except: [:index, :new, :create] Does this need to be replicated?
         
         describe "GET #index" do
           it { expect(get(:index)).to redirect_to(new_user_session_path) }
         end
   
-        # describe "GET #show" do
-        #   let(:resource_category) { create(:resource_category) }
-        # #   it { expect(get(:show)).to be_successful } #? empty controller?
-        # end
 
         describe "GET #new" do
           it { expect(get(:new)).to redirect_to(new_user_session_path) }
@@ -94,11 +88,10 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
       end
   
-  
+      #ADMIN
       context 'as a admin user' do
         let(:user) { create(:user, :admin) }
         before(:each) { sign_in(user) }
-        #before_action :set_resource_category, except: [:index, :new, :create] Does this need to be replicated?
 
         describe "GET #index" do
           it { expect(get(:index)).to be_successful } #what about checking in order?
