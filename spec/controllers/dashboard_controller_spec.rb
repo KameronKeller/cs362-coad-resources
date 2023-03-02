face7c708471560e2265288344d5db659070b741
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-01 21:47:50 -0800

    Write dashboard controller tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,43 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+    context 'as a logged-in user' do
+        let(:user) { create(:user) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to be_successful }
+        end
+    end
+
+    context 'as a user with an approved organization' do
+        let(:user) { create(:user, :organization_approved ) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to be_successful }
+        end
+    end
+    
+    context 'as a logged-out user' do
+        let(:user) { create(:user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(new_user_session_path) }
+        end
+    end
+
+
+    context 'as a admin user' do
+        let(:user) { create(:user, :admin) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to be_successful }
+        end
+
+        # describe ""
+        # post(:create, params: { resource_category: attributes_for(:resource_category) })
+    end
+
 end
