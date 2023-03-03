4f333e0494e6c5420f952fada4251d1b26bbd43c
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-03 08:01:28 -0800

    Comment incomplete test

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
-    it 'can be done with a new user' do
-        # Login with an unapproved user
-        user = create(:user)
-        # organization = create(:organization)
-        log_in_as(user)
+    # it 'can be done with a new user' do
+    #     # Login with an unapproved user
+    #     user = create(:user)
+    #     # organization = create(:organization)
+    #     log_in_as(user)
 
-        # Click "Create Application"
-        click_on 'Create Application'
-        # Check "I agree" on all items 
-        choose()
-        # Enter name
-        # Enter organization name
-        # enter title
-        # enter phone number
-        # contact
-        # secondary phone
-        # organization email
-        # check a box for donating
-        # enter a description
-        # check transportation button
-    end
+    #     # Click "Create Application"
+    #     click_on 'Create Application'
+    #     # Check "I agree" on all items 
+    #     choose()
+    #     # Enter name
+    #     # Enter organization name
+    #     # enter title
+    #     # enter phone number
+    #     # contact
+    #     # secondary phone
+    #     # organization email
+    #     # check a box for donating
+    #     # enter a description
+    #     # check transportation button
+    # end
 
 end
