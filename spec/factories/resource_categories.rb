1969495fb918570c54ab87e0b94cba96d38f20ee
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-07 10:50:10 -0800

    Generate name with sequence

--- spec/factories/resource_categories.rb
+++ spec/factories/resource_categories.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :resource_category do
-    name { 'FakeCategory' }
+    # name { 'FakeCategory' }
+    sequence :name do |n|
+      "FakeCategory#{n}"
+    end
     active { true }
   end
 end
